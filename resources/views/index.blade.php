a942342e58f6e6c7e773b052d1cdcbc4f2d70806
zbuqi <517851416@qq.com>
2022-12-26 00:21:11 +0800

    首页专题 赛程

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,39 +5,39 @@
 @section('pagename', 'home-page')
 
 @section('content')
-    <section class="zhuanti">
-        <div class="col-1-x padd-12">
-            <div class="zhuanti-main padd-12 clearfix">
+    <section class="zhuanti clearfix">
+        <div class="col-1-x">
+            <div class="zhuanti-main clearfix">
                 <h2 class="title padd-4">2022卡塔尔世界杯</h2>
                 <div class="col-2 padd-4">
-                    <div class="zt-item">
+                    <a class="zt-item icon-1" href="">
                         <div class="item-title">世界杯解读</div>
                         <p>专家助你红单</p>
-                    </div>
+                    </a>
                 </div>
                 <div class="col-2 padd-4">
-                    <div class="zt-item">
+                    <a class="zt-item icon-2" href="">
                         <div class="item-title">大神排行</div>
                         <p>世界杯擂台赛</p>
-                    </div>
+                    </a>
                 </div>
                 <div class="col-2 padd-4">
-                    <div class="zt-item">
+                    <a class="zt-item icon-3" href="">
                         <div class="item-title">夺冠指数</div>
                         <p>冠军实时更新</p>
-                    </div>
+                    </a>
                 </div>
                 <div class="col-2 padd-4">
-                    <div class="zt-item">
+                    <a class="zt-item icon-4" href="">
                         <div class="item-title">32强解密</div>
                         <p>红单密码在手</p>
-                    </div>
+                    </a>
                 </div>
                 <div class="col-1 padd-4">
-                    <div class="zt-zb-item">
+                    <a class="zt-zb-item icon-5" href="">
                         <div class="item-title">比分直播</div>
                         <p>世界杯赛事动画比分直播</p>
-                    </div>
+                    </a>
                 </div>
                 <div class="col-1 padd-4">
                     <div class="zhuanti-btn">
@@ -46,9 +46,191 @@
                 </div>
             </div>
         </div>
-        <div class="col-1-b padd-12">
+        <div class="col-1-b">
             <div class="zhuanti-banner">
-                <a href=""><img src="/img/zt-banner.png"></a>
+                <a href="/"><img src="/img/zt-banner.png"></a>
+            </div>
+        </div>
+    </section>
+    <section class="saic clearfix">
+        <div class="col-1-b">
+            <div class="saic-main">
+                <div class="saic-title">
+                    <h2 class="title">每天赛程</h2>
+                    <a href="">全部比赛 ></a>
+                </div>
+                <div class="content">
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr class="sai-item">
+                                <td rowspan="2">
+                                    <div class="time">
+                                        <p>12月03日</p>
+                                        <p>周日 23: 00</p>
+                                    </div>
+                                    <div class="">已完场</div>
+                                    <div class="sai-user-1">
+                                        <div class="name">克罗地亚</div>
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="sai-user-2">
+                                        <img src="" alt="">
+                                        <div class="name">巴西</div>
+                                    </div>
+                                    <div class="sai-video"></div>
+                                </td>
+                                <td>0</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td>
+                                <td rowspan="2"><a href="">72位专家推荐 ></a></td>
+                            </tr>
+                            <tr class="">
+                                <td>+1</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td> 
+                            </tr>
+                            <tr class="sai-item">
+                                <td rowspan="2">
+                                    <div class="time">
+                                        <p>12月03日</p>
+                                        <p>周日 23: 00</p>
+                                    </div>
+                                    <div class="">已完场</div>
+                                    <div class="sai-user-1">
+                                        <div class="name">克罗地亚</div>
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="sai-user-2">
+                                        <img src="" alt="">
+                                        <div class="name">巴西</div>
+                                    </div>
+                                    <div class="sai-video"></div>
+                                </td>
+                                <td>0</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td>
+                                <td rowspan="2"><a href="">72位专家推荐 ></a></td>
+                            </tr>
+                            <tr class="">
+                                <td>+1</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td> 
+                            </tr>
+                            <tr class="sai-item">
+                                <td rowspan="2">
+                                    <div class="time">
+                                        <p>12月03日</p>
+                                        <p>周日 23: 00</p>
+                                    </div>
+                                    <div class="">已完场</div>
+                                    <div class="sai-user-1">
+                                        <div class="name">克罗地亚</div>
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="sai-user-2">
+                                        <img src="" alt="">
+                                        <div class="name">巴西</div>
+                                    </div>
+                                    <div class="sai-video"></div>
+                                </td>
+                                <td>0</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td>
+                                <td rowspan="2"><a href="">72位专家推荐 ></a></td>
+                            </tr>
+                            <tr class="">
+                                <td>+1</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td> 
+                            </tr>
+                            <tr class="sai-item">
+                                <td rowspan="2">
+                                    <div class="time">
+                                        <p>12月03日</p>
+                                        <p>周日 23: 00</p>
+                                    </div>
+                                    <div class="">已完场</div>
+                                    <div class="sai-user-1">
+                                        <div class="name">克罗地亚</div>
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="sai-user-2">
+                                        <img src="" alt="">
+                                        <div class="name">巴西</div>
+                                    </div>
+                                    <div class="sai-video"></div>
+                                </td>
+                                <td>0</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td>
+                                <td rowspan="2"><a href="">72位专家推荐 ></a></td>
+                            </tr>
+                            <tr class="">
+                                <td>+1</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td> 
+                            </tr>
+                            <tr class="sai-item">
+                                <td rowspan="2">
+                                    <div class="time">
+                                        <p>12月03日</p>
+                                        <p>周日 23: 00</p>
+                                    </div>
+                                    <div class="">已完场</div>
+                                    <div class="sai-user-1">
+                                        <div class="name">克罗地亚</div>
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="sai-user-2">
+                                        <img src="" alt="">
+                                        <div class="name">巴西</div>
+                                    </div>
+                                    <div class="sai-video"></div>
+                                </td>
+                                <td>0</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td>
+                                <td rowspan="2"><a href="">72位专家推荐 ></a></td>
+                            </tr>
+                            <tr class="">
+                                <td>+1</td>
+                                <td><span>8.10</span><span>4.50</span><span>1.26</span></td> 
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-1-x">
+            <div class="saic-sidebar">
+                <div class="saic-sidebar-title">
+                    <div class="title">
+                        <span>夺冠指数</span>
+                        <span>实时更新</span>
+                    </div>
+                    <a href="/" alt="">全部 ></a>
+                </div>
+                <div class="saic-sidebar-content">
+                    <div class="col-2">
+                        <div class="content-item">
+                            <img src="" alt="">
+                            <span>克罗地亚</span>
+                            <span>2.60</span>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="content-item">
+                            <img src="" alt="">
+                            <span>克罗地亚</span>
+                            <span>2.60</span>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="content-item">
+                            <img src="" alt="">
+                            <span>克罗地亚</span>
+                            <span>2.60</span>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="content-item">
+                            <img src="" alt="">
+                            <span>克罗地亚</span>
+                            <span>2.60</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
