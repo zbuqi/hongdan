83c4d76d901fba7502b58613016c954300f96895
zbuqi <517851416@qq.com>
2022-12-31 01:43:57 +0800

    资讯列表

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -4,5 +4,194 @@
 @section('pagename', 'category-page')
 
 @section('content')
-    <div class="">这是分类页</div>
+    <div class="categoty">
+        <div class="breadcrumbs">
+            <a class="back" href="/"><span>←</span>首页</a>
+            <span>></span>
+            资讯
+        </div>
+        <div class="category-wrap clearfix">
+            <div class="col-4-b">
+                <div class="category-content">
+                    <div class="article-item">
+                        <div class="content clearfix">
+                            <a class="thumb" href="/" title=""><img src="/img/thumb-1.png" alt=""></a>
+                            <div class="info">
+                                <h2 class="title"><a href="/" title="">克罗地亚0-0比利时 欧洲红魔回家</a></h2>
+                                <p class="excerpt">摘要：盛世商品交易中心有限公司正规吗？韩有为吹嘘带单茅台酒、茅台醇典藏酒靠谱吗？老师为什么让我一直爆仓？盛世商品交易中心有限公司合法吗？难道是吃客户头寸所以一直亏损？难道是骗局吗？ 百人千股转投外汇，股指，配资策略，期权，期货，大宗商品等背...</p>
+                            </div>
+                        </div>
+                        <div class="meta">
+                            <a class="post-like" href="">赞同<span>156</span></a>
+                            <a class="comments" href="">147条评论</a>
+                            <a class="collect" href="/">收藏</a>
+                            <div class="meta-right">
+                                <span class="author">新京报社官方账号</span>
+                                <span class="time">2022-12-07</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="article-item">
+                        <div class="content clearfix">
+                            <a class="thumb" href="/" title=""><img src="/img/thumb-2.png" alt=""></a>
+                            <div class="info">
+                                <h2 class="title"><a href="/" title="">克罗地亚0-0比利时 欧洲红魔回家</a></h2>
+                                <p class="excerpt">摘要：盛世商品交易中心有限公司正规吗？韩有为吹嘘带单茅台酒、茅台醇典藏酒靠谱吗？老师为什么让我一直爆仓？盛世商品交易中心有限公司合法吗？难道是吃客户头寸所以一直亏损？难道是骗局吗？ 百人千股转投外汇，股指，配资策略，期权，期货，大宗商品等背...</p>
+                            </div>
+                        </div>
+                        <div class="meta">
+                            <a class="post-like" href="">赞同<span>156</span></a>
+                            <a class="comments" href="">147条评论</a>
+                            <a class="collect" href="/">收藏</a>
+                            <div class="meta-right">
+                                <span class="author">新京报社官方账号</span>
+                                <span class="time">2022-12-07</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="article-item">
+                        <div class="content clearfix">
+                            <a class="thumb" href="/" title=""><img src="/img/thumb-3.png" alt=""></a>
+                            <div class="info">
+                                <h2 class="title"><a href="/" title="">克罗地亚0-0比利时 欧洲红魔回家</a></h2>
+                                <p class="excerpt">摘要：盛世商品交易中心有限公司正规吗？韩有为吹嘘带单茅台酒、茅台醇典藏酒靠谱吗？老师为什么让我一直爆仓？盛世商品交易中心有限公司合法吗？难道是吃客户头寸所以一直亏损？难道是骗局吗？ 百人千股转投外汇，股指，配资策略，期权，期货，大宗商品等背...</p>
+                            </div>
+                        </div>
+                        <div class="meta">
+                            <a class="post-like" href="">赞同<span>156</span></a>
+                            <a class="comments" href="">147条评论</a>
+                            <a class="collect" href="/">收藏</a>
+                            <div class="meta-right">
+                                <span class="author">新京报社官方账号</span>
+                                <span class="time">2022-12-07</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="article-item">
+                        <div class="content clearfix">
+                            <a class="thumb" href="/" title=""><img src="/img/thumb-1.png" alt=""></a>
+                            <div class="info">
+                                <h2 class="title"><a href="/" title="">克罗地亚0-0比利时 欧洲红魔回家</a></h2>
+                                <p class="excerpt">摘要：盛世商品交易中心有限公司正规吗？韩有为吹嘘带单茅台酒、茅台醇典藏酒靠谱吗？老师为什么让我一直爆仓？盛世商品交易中心有限公司合法吗？难道是吃客户头寸所以一直亏损？难道是骗局吗？ 百人千股转投外汇，股指，配资策略，期权，期货，大宗商品等背...</p>
+                            </div>
+                        </div>
+                        <div class="meta">
+                            <a class="post-like" href="">赞同<span>156</span></a>
+                            <a class="comments" href="">147条评论</a>
+                            <a class="collect" href="/">收藏</a>
+                            <div class="meta-right">
+                                <span class="author">新京报社官方账号</span>
+                                <span class="time">2022-12-07</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="article-item">
+                        <div class="content clearfix">
+                            <a class="thumb" href="/" title=""><img src="/img/thumb-2.png" alt=""></a>
+                            <div class="info">
+                                <h2 class="title"><a href="/" title="">克罗地亚0-0比利时 欧洲红魔回家</a></h2>
+                                <p class="excerpt">摘要：盛世商品交易中心有限公司正规吗？韩有为吹嘘带单茅台酒、茅台醇典藏酒靠谱吗？老师为什么让我一直爆仓？盛世商品交易中心有限公司合法吗？难道是吃客户头寸所以一直亏损？难道是骗局吗？ 百人千股转投外汇，股指，配资策略，期权，期货，大宗商品等背...</p>
+                            </div>
+                        </div>
+                        <div class="meta">
+                            <a class="post-like" href="">赞同<span>156</span></a>
+                            <a class="comments" href="">147条评论</a>
+                            <a class="collect" href="/">收藏</a>
+                            <div class="meta-right">
+                                <span class="author">新京报社官方账号</span>
+                                <span class="time">2022-12-07</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="article-item">
+                        <div class="content clearfix">
+                            <a class="thumb" href="/" title=""><img src="/img/thumb-3.png" alt=""></a>
+                            <div class="info">
+                                <h2 class="title"><a href="/" title="">克罗地亚0-0比利时 欧洲红魔回家</a></h2>
+                                <p class="excerpt">摘要：盛世商品交易中心有限公司正规吗？韩有为吹嘘带单茅台酒、茅台醇典藏酒靠谱吗？老师为什么让我一直爆仓？盛世商品交易中心有限公司合法吗？难道是吃客户头寸所以一直亏损？难道是骗局吗？ 百人千股转投外汇，股指，配资策略，期权，期货，大宗商品等背...</p>
+                            </div>
+                        </div>
+                        <div class="meta">
+                            <a class="post-like" href="">赞同<span>156</span></a>
+                            <a class="comments" href="">147条评论</a>
+                            <a class="collect" href="/">收藏</a>
+                            <div class="meta-right">
+                                <span class="author">新京报社官方账号</span>
+                                <span class="time">2022-12-07</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4-x">
+                <div class="article-sidebar">
+                    <div class="sidebar-title">
+                        <div class="title"><span>热门推荐</span></div>
+                    </div>
+                    <div class="sidebar-content">
+                        <div class="article-sidebar-item">
+                            <a class="thumb" href=""><img src="/img/thumb-2.png"></a>
+                            <div class="info">
+                                <div class="title">
+                                    <a href="/">荷乙17轮前瞻：兹沃勒迎来昔日克星...</a>
+                                </div>
+                                <div class="meta">
+                                    <span>体育发飙季</span>
+                                    <span>3评论</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="article-sidebar-item">
+                            <a class="thumb" href=""><img src="/img/thumb-2.png"></a>
+                            <div class="info">
+                                <div class="title">
+                                    <a href="/">荷乙17轮前瞻：兹沃勒迎来昔日克星...</a>
+                                </div>
+                                <div class="meta">
+                                    <span>体育发飙季</span>
+                                    <span>3评论</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="article-sidebar-item">
+                            <a class="thumb" href=""><img src="/img/thumb-2.png"></a>
+                            <div class="info">
+                                <div class="title">
+                                    <a href="/">荷乙17轮前瞻：兹沃勒迎来昔日克星...</a>
+                                </div>
+                                <div class="meta">
+                                    <span>体育发飙季</span>
+                                    <span>3评论</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="article-sidebar-item">
+                            <a class="thumb" href=""><img src="/img/thumb-2.png"></a>
+                            <div class="info">
+                                <div class="title">
+                                    <a href="/">荷乙17轮前瞻：兹沃勒迎来昔日克星...</a>
+                                </div>
+                                <div class="meta">
+                                    <span>体育发飙季</span>
+                                    <span>3评论</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="article-sidebar-item">
+                            <a class="thumb" href=""><img src="/img/thumb-2.png"></a>
+                            <div class="info">
+                                <div class="title">
+                                    <a href="/">荷乙17轮前瞻：兹沃勒迎来昔日克星...</a>
+                                </div>
+                                <div class="meta">
+                                    <span>体育发飙季</span>
+                                    <span>3评论</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
