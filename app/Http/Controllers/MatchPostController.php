e7e2063ad73842fb967a6833d25c394832e3c582
zbuqi <517851416@qq.com>
2023-02-21 01:19:10 +0800

    数据

--- app/Http/Controllers/MatchPostController.php
+++ app/Http/Controllers/MatchPostController.php
@@ -6,27 +6,50 @@ use App\Http\Controllers\Controller;
 use App\Models\Match;
 use App\Models\ApiMatchs;
 use App\Http\Repositories\Lottery;
-use App\Http\Repositories\Matchs as MatchsApi;
+use App\Http\Repositories\Matchs;
+use App\Http\Repositories\MatchsApi;
 
 class MatchPostController extends Controller
 {
     public function show(){
+
         $lottery = Lottery::index(101,1);
-        $issues = [];
-        $match_ids = [];
+        $post_data = [];
         for($i=0;$i<count($lottery);$i++){
-            if(!in_array($lottery[$i]->issue,$issues)){
-                $issues[] = $lottery[$i]->issue;
-            }
-            for($x=0; $x<count($issues); $x++){
-                if($lottery[$i]->issue == $issues[$x]){
-                    $match_ids[$issues[$x]][] = $lottery[$i]->match_id;
+            $match = ApiMatchs::where("match_id", $lottery[$i]->match_id)->first();
+            if($match != ""){
+                $data = json_decode($match->value,true);
+                #体彩关联数据
+                $data["sport_id"] = $lottery[$i]->sport_id;
+                $data["lottery_type"] = $lottery[$i]->lottery_type;
+                $data["issue"] = $lottery[$i]->issue;
+                $data["lottery_id"] = $lottery[$i]->id;
+                $data["is_same"] = $lottery[$i]->is_same;
+                $post_data[$i] = $data;
+                #判断是否有阵容
+                if($data["coverage"]["lineup"] == 1){
+                    #阵容
+                    $lineup = MatchsApi::lineup($data->id);
+                    $data["lineup_confirmed"] = $lineup->confirmed;
+                    $data["home_formation"] = $lineup->home_formation;
+                    $data["away_formation"] = $lineup->away_formation;
+                    $data["lineup_home"] = json_encode($lineup->home, JSON_UNESCAPED_UNICODE);
+                    $data["lineup_away"] = json_encode($lineup->away, JSON_UNESCAPED_UNICODE);
+                }else{
+                    $data["lineup_confirmed"] = "";
+                    $data["home_formation"] = "";
+                    $data["away_formation"] = "";
+                    $data["lineup_home"] = "";
+                    $data["lineup_away"] = "";
                 }
             }
-            
-            echo "序号：" . $i . "比赛Id：" . $lottery[$i]->match_id . "， 彩票期号：" . $lottery[$i]->issue . "， 主队名称：" . $lottery[$i]->home_name . "， 客队名称：" . $lottery[$i]->away_name . "<br>";
         }
-        //print_r(MatchsApi::update_api_matchs(20230225));
+        #print_r($post_data);
+        #Match::insert($post_data);
+
+        #print_r(Matchs::update_api_matchs(20230228));
+
+        #ApiMatchs::where("match_time",">=",1677513600)->delete();
     }
 
 }
--- app/Http/Repositories/Matchs.php
+++ app/Http/Repositories/Matchs.php
@@ -11,14 +11,17 @@ class Matchs
         $data = MatchsApi::matchs($date);
         /*判断是否出错，出错返回出错信息*/
         if(property_exists($data, "err")){
-            return $data->err;
+            return $data;
+        }
+        if(property_exists($data, "msg")){
+            return $data;
         }
         $matchs = $data->results->match;
         $competitions = $data->results->competition;
         $teams = $data->results->team;
         $status = Seting::where('name','match-status')->first()->value;
         $status = json_decode($status);
-        
+
         for($i=0;$i<count($matchs);$i++){
             /*星期*/
             $week = array('天','一','二','三','四','五','六');
@@ -52,18 +55,23 @@ class Matchs
     }
     public function update_api_matchs($date){
         $matchs = Matchs::show($date);
+        $time = date("Y-m-d H:i:s",time());
         $post_data = [];
-        for($i=0;$i<count($matchs);$i++){
+        for($i=0;$i<count($matchs);$i++) {
             $post_data[$i]["match_id"] = $matchs[$i]->id;
             $post_data[$i]["match_time"] =  $matchs[$i]->match_time;
             $post_data[$i]["value"] = json_encode($matchs[$i], JSON_UNESCAPED_UNICODE);
+            $post_data[$i]["diary"] = $date;
+            $post_data[$i]["updated_at"] = $time;
+            $post_data[$i]["created_at"] = $time;
         }
+
         $query_date = ApiMatchs::where("match_time",">=",strtotime($date))->where("match_time","<",strtotime($date)+86400)->first();
         /*判断数据库是否有传入date时间的赛程数据，没有，就插入date时间的赛程数据列表*/
         if($query_date == ""){
             if(ApiMatchs::insert($post_data)){
                 echo "数据插入成功";
-                /*删除10天前的数据*/
+                #删除10天前的数据
                 $delete_date = strtotime($date) - 15*86400;
                 if(ApiMatchs::where("match_time","<",$delete_date)->first() != ""){
                     echo ApiMatchs::where("match_time","<",$delete_date)->delete();
@@ -71,7 +79,9 @@ class Matchs
             }
         }else{
             echo "数据已经存在";
+            //ApiMatchs::truncate();
         }
     }
+
 }
 
